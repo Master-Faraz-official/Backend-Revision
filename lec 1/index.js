1ebff8d7fa49265a613b02a665f724dba8caaca8
Faraz ALi Ahmad <farazofficial9028@gmail.com>
2025-01-18 09:16:38 +0530

    Backend Revision

---
+++ lec 1/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const port = 4000
+
+app.get("/", (req, res) => {
+    res.send("This is the revision part")
+})
+
+app.get("/test", (req, res) => {
+    res.send("This is the test part")
+})
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`)
+}) 
